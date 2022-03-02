f05fd4c7f18b3009eb88755f5647fb2f1757d9d7
Ben Lewis <bl3e20@soton.ac.uk>
2022-03-02 20:21:16 +0000

    Runway Calculations
    Added all new  runway calculation functions. All of these do assume that the obstacle is a telephone pole  but this can be updated in a later version

--- src/main/java/uk/ac/soton/comp2211/utility/Calculator.java
+++ src/main/java/uk/ac/soton/comp2211/utility/Calculator.java
@@ -21,15 +21,50 @@ public class Calculator {
 
     public Runway LandingOverObstacle (Runway runway, ObstacleOnRunway obs){
         Runway runwayWithObstacle = new Runway(runway);
-        int obstaclePosition = obs.getPosition(); //obstacle position should represent how far down the runway it is
-        int obstacleLength = 2;
+        int obstaclePosition = obs.getPosition(); //obstacle position should represent how far down the runway  the highest point is
+        int obstacleLength = obs.getLength();
         int RESA = 240;
         int distanceToStripEnd = 60;
-        int newLDA = obstaclePosition -(RESA + distanceToStripEnd);
+        int obstacleHeight = obs.getHeight();
+        //int newLDA = (obstaclePosition -distanceToStripEnd) - Integer.max((RESA + obstacleLength) , (obstacleHeight *50));
+        int newLDA = runway.getLength() -obstaclePosition -distanceToStripEnd -  (obstacleHeight *50);
         runwayWithObstacle.setLDA(newLDA);
 
 
         return runwayWithObstacle;
     }
 
+    public Runway TakeOffTowardsObstacle (Runway runway, ObstacleOnRunway obs){
+        Runway runwayWithObstacle = new Runway(runway);
+        int obstacleHeight = obs.getHeight();
+        int obstaclePosition = obs.getPosition(); //obstacle position should represent how far down the runway it is
+        int RESA = 240;
+        int distanceToStripEnd = 60;
+        int clearway = runway.getTODA() -runway.getTORA();
+        int stopway = runway.getASDA() - runway.getTORA();
+        int newTORA = (obstaclePosition + runway.getDTL()) -(obstaclePosition -distanceToStripEnd) -  (obstacleHeight *50);
+        runwayWithObstacle.setTORA(newTORA);
+        runwayWithObstacle.setTODA(newTORA+clearway);
+        runwayWithObstacle.setASDA(newTORA+stopway);
+
+
+        return runwayWithObstacle;
+    }
+
+    public Runway TakeOffAwayFromObstacle (Runway runway, ObstacleOnRunway obs){
+        Runway runwayWithObstacle = new Runway(runway);
+        int obstacleHeight = obs.getHeight();
+        int obstaclePosition = obs.getPosition(); //obstacle position should represent how far down the runway it is
+        int RESA = 240;
+        int distanceToStripEnd = 60;
+        int clearway = runway.getTODA() -runway.getTORA();
+        int stopway = runway.getASDA() - runway.getTORA();
+        int newTORA = obstaclePosition -runway.getDTL() -(obstaclePosition -distanceToStripEnd) -  (obstacleHeight *50);
+        runwayWithObstacle.setTORA(newTORA);
+        runwayWithObstacle.setTODA(newTORA);
+        runwayWithObstacle.setASDA(newTORA);
+
+
+        return runwayWithObstacle;
+    }
 }
