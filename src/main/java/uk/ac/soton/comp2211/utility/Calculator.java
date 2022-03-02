6ddd10e711d5ed3eb825cfefe7de206f4c003a54
Ben Lewis <bl3e20@soton.ac.uk>
2022-03-02 17:54:59 +0000

    First Runway Calculation Added
    Also added a new constructor to runway

--- src/main/java/uk/ac/soton/comp2211/utility/Calculator.java
+++ src/main/java/uk/ac/soton/comp2211/utility/Calculator.java
@@ -1,5 +1,24 @@
 package uk.ac.soton.comp2211.utility;
 
+<<<<<<< Updated upstream
 public class Calculator {
     
+=======
+import uk.ac.soton.comp2211.components.ObstacleOnRunway;
+import uk.ac.soton.comp2211.components.Runway;
+
+public class Calculator {
+
+   public Runway LandingTowardsObstacle (Runway runway, ObstacleOnRunway obs){
+       Runway runwayWithObstacle = new Runway(runway);
+       int obstaclePosition = obs.getPosition();
+       int RESA = 240;
+       int distanceToStripEnd = 60;
+       int newLDA = obstaclePosition -(RESA + distanceToStripEnd);
+       runwayWithObstacle.setLDA(newLDA);
+
+
+       return runwayWithObstacle;
+   }
+>>>>>>> Stashed changes
 }
