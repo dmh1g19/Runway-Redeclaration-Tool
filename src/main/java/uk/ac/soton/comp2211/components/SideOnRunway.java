808e5d7a09a6f22a15283899f7ab4989646f7bce
JPadden <jp3g20@soton.ac.uk>
2022-03-04 13:41:46 +0000

    Side-On-View base

--- src/main/java/uk/ac/soton/comp2211/components/SideOnRunway.java
+++ src/main/java/uk/ac/soton/comp2211/components/SideOnRunway.java
@@ -1,9 +1,35 @@
 package uk.ac.soton.comp2211.components;
 
-public class SideOnRunway {
+public class SideOnRunway extends Canvas{
 
     /**
      * Draw the view for side-on runway here -
      */
+    private final Runway runway;
+    private final ObstacleOnRunway obstacle;
+    private final double width;
+    private final double height;
+
+    public SideOnRunway(Runway runway, ObstacleOnRunway obstacle, double width, double height){
+        this.runway = runway;
+        this.obstacle = obstacle;
+        this.width = width;
+        this.height = height;
+
+        setWidth(width);
+        setHeight(height);
+
+        representView();
+
+        //add listener so that everytime runway or obstacle changed by user side on view updates.
+    }
+
+    private void representView(){
+        var gc = getGrapicsContext2D();
+        gc.clearRect(0,0,width,height);
+
+    }
+
+
 
 }
