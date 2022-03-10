ee44aec6d5baab724b5e51287415131bbf7838b1
zw9n20 <zw9n20@soton.ac.uk>
2022-03-10 11:18:08 +0000

    Update XMLUtil.java

--- src/main/java/uk/ac/soton/comp2211/utility/XMLUtil.java
+++ src/main/java/uk/ac/soton/comp2211/utility/XMLUtil.java
@@ -2,17 +2,85 @@ package uk.ac.soton.comp2211.utility;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import uk.ac.soton.comp2211.airport.Airport;
 
+import uk.ac.soton.comp2211.airport.Airport;
+import uk.ac.soton.comp2211.airport.Runway;
 public class XMLUtil {
     
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("airports.xml");
         Airport obj1 = (Airport) context.getBean("Airport1");
         Airport obj2 = (Airport) context.getBean("Airport2");
+        Runway RW1 =(Runway) context.getBean("Runway1");
+        Runway RW2 =(Runway) context.getBean("Runway2");
+        Runway RW3 =(Runway) context.getBean("Runway3");
+        Runway RW4 =(Runway) context.getBean("Runway4");
+        Runway RW5 =(Runway) context.getBean("Runway5");
+        Runway RW6 =(Runway) context.getBean("Runway6");
         obj1.getName();
         obj1.getRunways();
         obj2.getName();
-        obj2.getRunways();  
+        obj2.getRunways();
+
+        RW1.getName();
+        RW1.getLength();
+        RW1.getWidth();
+        RW1.getBearing();
+        RW1.getTODA();
+        RW1.getASDA();
+        RW1.getLDA();
+        RW1.getDTL();
+        RW1.getClearwayLength();
+
+        RW2.getName();
+        RW2.getLength();
+        RW2.getWidth();
+        RW2.getBearing();
+        RW2.getTODA();
+        RW2.getASDA();
+        RW2.getLDA();
+        RW2.getDTL();
+        RW2.getClearwayLength();
+
+        RW3.getName();
+        RW3.getLength();
+        RW3.getWidth();
+        RW3.getBearing();
+        RW3.getTODA();
+        RW3.getASDA();
+        RW3.getLDA();
+        RW3.getDTL();
+        RW3.getClearwayLength();
+
+        RW4.getName();
+        RW4.getLength();
+        RW4.getWidth();
+        RW4.getBearing();
+        RW4.getTODA();
+        RW4.getASDA();
+        RW4.getLDA();
+        RW4.getDTL();
+        RW4.getClearwayLength();
+
+        RW5.getName();
+        RW5.getLength();
+        RW5.getWidth();
+        RW5.getBearing();
+        RW5.getTODA();
+        RW5.getASDA();
+        RW5.getLDA();
+        RW5.getDTL();
+        RW5.getClearwayLength();
+
+        RW6.getName();
+        RW6.getLength();
+        RW6.getWidth();
+        RW6.getBearing();
+        RW6.getTODA();
+        RW6.getASDA();
+        RW6.getLDA();
+        RW6.getDTL();
+        RW6.getClearwayLength();
         }
 }
+
