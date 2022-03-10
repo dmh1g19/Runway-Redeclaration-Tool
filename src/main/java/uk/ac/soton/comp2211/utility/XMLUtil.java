987e2a0678cf47497fc393086409bab416fbdfd5
zw9n20 <zw9n20@soton.ac.uk>
2022-03-10 03:31:34 +0000

    Update XMLUtil.java

--- src/main/java/uk/ac/soton/comp2211/utility/XMLUtil.java
+++ src/main/java/uk/ac/soton/comp2211/utility/XMLUtil.java
@@ -1,55 +1,18 @@
 package uk.ac.soton.comp2211.utility;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.IOException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import uk.ac.soton.comp2211.airport.Airport;
 
 public class XMLUtil {
-
-    //TODO: pass filename through user input
-    String fileName = "airports.xml";
-    String directory = "src/main/resources/uk/ac/soton/comp2211/" + fileName;
-
-    DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
-
-    public void scanDocument() throws Exception {
-        try {
-            DocumentBuilder build = fac.newDocumentBuilder();
-            Document document = build.parse(directory);
-
-            NodeList nodeList = document.getElementsByTagName("runway");
-
-            for(int i=0;i<nodeList.getLength();i++) {
-                Node curNode = nodeList.item(i);
-
-                if(curNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) curNode;
-
-                    //TODO: pass to runway/airport class, get airport name?
-                    String name = getElementContentStr(element, "name");
-                    String TORA = getElementContentStr(element, "TORA");
-                    String TODA  = getElementContentStr(element, "TODA");
-                    String ASDA = getElementContentStr(element, "ASDA");
-                    String LDA = getElementContentStr(element, "LDA");
-                    String stripLength = getElementContentStr(element, "stripLength");
-                    String stripWidth = getElementContentStr(element, "stripWidth");
-                    String clearwayLength = getElementContentStr(element, "clearwayLength");
-                    String clearwayWidth = getElementContentStr(element, "clearwayWidth");
-                }
-            }
-        } catch (SAXException | IOException e) {
-            e.printStackTrace();
+    
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("airports.xml");
+        Airport obj1 = (Airport) context.getBean("Airport1");
+        Airport obj2 = (Airport) context.getBean("Airport2");
+        obj1.getName();
+        obj1.getRunways();
+        obj2.getName();
+        obj2.getRunways();  
         }
-    }
-
-    public String getElementContentStr (Element element, String attr) {
-        Node node = element.getElementsByTagName(attr).item(0);
-        return node.getTextContent();
-    }
 }
